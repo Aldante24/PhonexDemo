9d2f53425c115b9df21e6bb276a45a4d22f9caa0
Aldo Martinez Amador <mtz.aldo24@gmail.com>
2017-05-05 18:55:10 -0500

    Iniciando Repo

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="es" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link rel="icon" type="image/png" href="images/icon 2.png" />
+    <title>Phonex | Contact Center</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="es" />
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="style0.css" rel="stylesheet" type="text/css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script type="text/javascript">
+    //<![CDATA[
+    var j = jQuery.noConflict();
+    j(function (){
+    j(".esthela").hover(function(){
+    j(".esthela").stop(true, false).animate({right:"-130"},"medium");
+    },function(){
+    j(".esthela").stop(true, false).animate({right:"-375"},"medium");
+    },500);
+    return false;
+    });
+    //]]>
+    </script>
+    <script type="text/javascript">
+    //<![CDATA[
+    var j = jQuery.noConflict();
+    j(function (){
+    j(".esthelas").hover(function(){
+    j(".esthelas").stop(true, false).animate({right:"-130"},"medium");
+    },function(){
+    j(".esthelas").stop(true, false).animate({right:"-375"},"medium");
+    },500);
+    return false;
+    });
+    //]]>
+    </script>
+</head>
+
+<body>
+    <div id="page">
+        <div id="header">
+            <h1><a href="#">Phonex</a></h1>
+            <div id="menu">
+                <ul>
+                    <li><a class="active" href="index.php">Inicio</a></li>
+                    <!-- <li><a href="#">Products</a></li>
+                    <li><a href="#">Gallery</a></li> -->
+                    <li><a href="contacto.php">Contacto</a></li>
+                    <li><a href="elastix.html">Elastix</a></li>
+                    <!-- <li><a href="#" onClick="MyWindow=window.open('https://10.10.2.250/','MyWindow',width=600,height=300); return false;">Elastix</a></li> -->
+                </ul>
+            </div>
+        </div>
+        <div id="intro">
+            <h2>Nuestra Historia</h2>
+        <p>Somos una empresa dedicada a brindar servicios de call center/contact center de acuerdo a sus necesidades. Ponemos a su disposición nuestra experiencia en procesos de adquisición y retención de clientes, así como nuestra infraestructura de misión critica 7x24. Dedicamos nuestro esfuerzo a exceder la expectativa del cliente a través del trabajo serio y comprometido pero también creativo e innovador.</p> </div>
+        <div id="main">
+			<div class="ic"></div>
+            <div id="sidebar">
+                <h2>Misión</h2>
+                <p><font color="black">Garantizar la satisfacción de nuestros clientes a través de la prestación de servicios de Centro de Contacto, vinculando a las empresas con sus usuarios, consumidores y proveedores; aportándoles valor y mejorando sus resultados, mediante la sinergia del talento humano, tecnología de vanguardia y procesos eficientes.</font></p>
+                <br>
+                <br>
+                <h2>Visión</h2>
+                  <p><font color="black">Consolidarnos como una empresa rentable reconocida en el mercado por su flexibilidad, rapidez y creatividad, que ofrezca a sus clientes soluciones efectivas, competitivas e innovadoras, brinde al personal oportunidades de crecimiento y cumpla las expectativas de los accionistas.</font></p>
+                <!-- <h2>Mauris sagittis</h2>
+                <div class="box">
+                    <p>Mauris sollicitudin tincidunt magna vitae semper. Curabitur ut pharetra quam. Integer rhoncus convallis urna vitae mattis.</p>
+                    <p>Aenean nec dui quis urna sollicitudin sodales. Fusce laoreet, ligula et rhoncus volutpat, felis magna varius tortor, ac molestie diam lorem in lectus.</p>
+                </div> -->
+            </div><!-- sidebar -->
+            <div id="content">
+                <div class="post">
+                    <h2><a href="#">Calidad y Seguridad de la Información</a></h2>
+                    <p class="postmeta">POLITICA</p>
+                    <div class="entry"><img src="images/image2.jpg" alt="" />
+                        <p>En PHONEX asumimos el compromiso con nuestros clientes de satisfacer plenamente sus requerimientos comerciales y contractuales en el servicio de Call Center a través de personal comprometido y técnicamente competente, que opere conforme a nuestro Sistema Integral de Gestión (SIG) bajo la normatividad ISO 9001:2008 e ISO 27001:2005.</p>
+                        <!-- <p>Under no circumstances can you use the template for websites with illegal or immoral (even if legal) content. This includes, but is not limited to pornography, obscenity, homosexuality, abortion, violence, drugs, gambling.</p>
+                        <p>The template contains public domain images from <a href="http://www.publicdomainpictures.net/">PublicDomainPictures.net</a> and <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikimedia Commons</a>. Although public domain pictures are free and with no obligation to credit their authors you should keep in mind that in some cases they may infringe trademark, property or any other rights of others. Therefore, it is you who has to decide whether to use the image, especially if your website will be a commercial one... <a href="#">more</a></p> -->
+                        <br>
+                        <br>
+                    </div>
+                </div>
+                <div class="post">
+                    <!-- <h2><a href="#">Lorem ipsum dolor sit amet</a></h2> -->
+                    <p class="postmeta">PROMOVIENDO</p>
+                    <div class="entry"><img src="images/image3.jpg" alt="" />
+                        <!-- <p>Quisque congue lacus sed odio fermentum tincidunt. Proin vitae nulla velit. Cras consectetur commodo scelerisque. Curabitur leo nisl, blandit at tempus et, interdum at risus.</p> -->
+                        <ul>
+                            <li>La mejora continua en los procesos a fin de ofrecer servicios oportunos en la atención, solución y operación de sus campañas.</li>
+                            <li>La identificación de las amenazas y vulnerabilidades para minimizar los riesgos a los cuales se expone la información.</li>
+                            <li>Mantener la implementación de un Sistema de Gestión de Seguridad de la información para la continuidad del negocio, la administración de riesgos y la consolidación de una cultura de seguridad. </li>
+                            <li>Preservar la confidencialidad, integridad y disponibilidad de la información que maneje en sus distintas áreas y procesos, con la finalidad de salvaguardar los datos del cliente y de la empresa.</li>
+                        </ul>
+                        <!-- <p>Sed dui augue, pellentesque ac pulvinar id, malesuada eget diam. Integer elementum sem eget tortor faucibus id pellentesque lorem dignissim... <a href="#">more</a></p> -->
+                    </div>
+                </div>
+            </div><!-- content -->
+            <div class="clearing">&nbsp;</div>
+        </div><!-- main -->
+        <div class="esthela" style="right: -375px;">
+          <div style="color: rgb(255, 255, 255); padding: 8px 5px 0pt 50px;">
+              <div class="">
+                  <?php include ('tellamamos.html');?>
+              </div>
+          </div>
+        </div>
+        <div class="esthelas" style="right: -375px;">
+          <div style="color: rgb(255, 255, 255); padding: 8px 5px 0pt 50px;">
+              <div class="">
+                  <?php include ('boton.html');?>
+              </div>
+          </div>
+        </div>
+        <!-- <div id="footer">
+            <div id="footerleft">
+                <p>Online: 7 viewers</p>
+            </div>
+            <div id="footerright">
+                <p>Copyright &copy; 2011, designed by <a href="http://www.webtemplateocean.com/">WebTemplateOcean.com</a></p>
+				<p>Busque m&aacute;s plantillas web gratis <a href="http://www.mejoresplantillasgratis.es" target="_blank">en MPG.es</a>.</p>
+            </div> -->
+            <!-- <script type="text/javascript">
+            var LHCChatOptions = {};
+            LHCChatOptions.opt = {widget_height:340,widget_width:300,popup_height:520,popup_width:500};
+            (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            var referrer = (document.referrer) ? encodeURIComponent(document.referrer.substr(document.referrer.indexOf('://')+1)) : '';
+            var location  = (document.location) ? encodeURIComponent(window.location.href.substring(window.location.protocol.length)) : '';
+            po.src = '//189.204.89.162/phchat/lhc_web/index.php/esp/chat/getstatus/(click)/internal/(position)/middle_right/(ma)/br/(top)/150/(units)/pixels/(leaveamessage)/true?r='+referrer+'&l='+location;
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+            })();
+            </script> -->
+            <div id="clearingfooter">&nbsp;</div>
+        </div><!-- footer -->
+    </div><!-- page -->
+</body>
+</html>
